d6dd56bfb9f7973b11ae9820de1d73928c0c4c1e
TOMOU <xorrhks@gmail.com>
2024-10-08 11:51:46 +0900

    타이틀화면 터치 시 씬 이동

--- Assets/Scripts/UI/Panel/Panel_Title.cs
+++ Assets/Scripts/UI/Panel/Panel_Title.cs
@@ -1,12 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
 public class Panel_Title : UIBase
 {
+    [SerializeField] private Button _touchButton;
+
     public override void Open()
     {
         base.Open();
+
+        _touchButton?.onClick.AddListener(OnClickTitle);
     }
 
     public override void Close()
     {
         base.Close();
+
+        _touchButton?.onClick.RemoveAllListeners();
+    }
+
+    private void OnClickTitle()
+    {
+        SceneManager.Instance.LoadScene("Scene_Lobby");
     }
 }
