fa5c211d06fb0a093bcadb1bd84485b8e14dc447
TOMOU <xorrhks@gmail.com>
2024-10-10 15:13:11 +0900

    로비 버튼에 함수 연결 (빈함수, 추후 작업예정)

--- Assets/Scripts/UI/Panel/Panel_Lobby.cs
+++ Assets/Scripts/UI/Panel/Panel_Lobby.cs
@@ -6,6 +6,7 @@ public class Panel_Lobby : UIBase
 {
     [Header("- 플레이어 정보 (좌상단)")]
     [SerializeField] private TextMeshProUGUI _textPlayerLevel;
+    [SerializeField] private TextMeshProUGUI _textPlayerName;
     [SerializeField] private Image _imgPlayerExpSlider;
     [SerializeField] private TextMeshProUGUI _textPlayerExpValue;
 
@@ -37,10 +38,137 @@ public class Panel_Lobby : UIBase
     public override void Open()
     {
         base.Open();
+
+        InitButton();
+        Refresh();
     }
 
     public override void Close()
     {
         base.Close();
+
+        ReleaseButton();
+    }
+
+    private void InitButton()
+    {
+        _btnNotice.onClick.AddListener(OnClickNotice);
+        _btnQuest.onClick.AddListener(OnClickQuest);
+        _btnDictionary.onClick.AddListener(OnClickDictionary);
+        _btnShop.onClick.AddListener(OnClickShop);
+
+        _btnCharacter.onClick.AddListener(OnClickCharacter);
+        _btnInventory.onClick.AddListener(OnClickInventory);
+        _btnStorybook.onClick.AddListener(OnClickStorybook);
+        _btnGuild.onClick.AddListener(OnClickGuild);
+        _btnGacha.onClick.AddListener(OnClickGacha);
+
+        _btnWorld.onClick.AddListener(OnClickWorld);
+
+        _btnPost.onClick.AddListener(OnClickPost);
+        _btnOption.onClick.AddListener(OnClickOption);
+    }
+
+    private void ReleaseButton()
+    {
+        _btnNotice?.onClick.RemoveAllListeners();
+        _btnQuest?.onClick.RemoveAllListeners();
+        _btnDictionary?.onClick.RemoveAllListeners();
+        _btnShop?.onClick.RemoveAllListeners();
+
+        _btnCharacter?.onClick.RemoveAllListeners();
+        _btnInventory?.onClick.RemoveAllListeners();
+        _btnStorybook?.onClick.RemoveAllListeners();
+        _btnGuild?.onClick.RemoveAllListeners();
+        _btnGacha?.onClick.RemoveAllListeners();
+
+        _btnWorld?.onClick.RemoveAllListeners();
+
+        _btnPost?.onClick.RemoveAllListeners();
+        _btnOption?.onClick.RemoveAllListeners();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Refresh()
+    {
+        _textPlayerLevel.text = "1";
+        _textPlayerName.text = "토모우";
+
+        int curExp = 103;
+        int maxExp = 1000;
+        _imgPlayerExpSlider.fillAmount = curExp / (float)maxExp;
+        _textPlayerExpValue.text = string.Format("{0}/{1}", curExp, maxExp);
+
+        int curStamina = 53;
+        int maxStamina = 65;
+        _textStaminaValue.text = string.Format("{0}/{1}", curStamina, maxStamina);
+
+        int curGold = 30000;
+        _textGoldValue.text = string.Format("{0:##,##0}", curGold);
+
+        int curCash = 0;
+        _textCashValue.text = string.Format("{0:##,##0}", curCash);
+    }
+
+    private void OnClickNotice()
+    {
+        Logger.Log("OnClickNotice");
+    }
+
+    private void OnClickQuest()
+    {
+        Logger.Log("OnClickQuest");
+    }
+
+    private void OnClickDictionary()
+    {
+        Logger.Log("OnClickDictionary");
+    }
+
+    private void OnClickShop()
+    {
+        Logger.Log("OnClickShop");
+    }
+
+    private void OnClickCharacter()
+    {
+        Logger.Log("OnClickCharacter");
+    }
+
+    private void OnClickInventory()
+    {
+        Logger.Log("OnClickInventory");
+    }
+
+    private void OnClickStorybook()
+    {
+        Logger.Log("OnClickStorybook");
+    }
+
+    private void OnClickGuild()
+    {
+        Logger.Log("OnClickGuild");
+    }
+
+    private void OnClickGacha()
+    {
+        Logger.Log("OnClickGacha");
+    }
+
+    private void OnClickWorld()
+    {
+        Logger.Log("OnClickWorld");
+    }
+
+    private void OnClickPost()
+    {
+        Logger.Log("OnClickPost");
+    }
+
+    private void OnClickOption()
+    {
+        Logger.Log("OnClickOption");
     }
 }
